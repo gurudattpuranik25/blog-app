4e5b08b629e52abdef019b82f1afb6ad7582fe64
gurudattpuranik25 <gurudattpuranik35@gmail.com>
2022-07-26 19:54:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div>
-      <h1 className=" text-3xl font-semibold text-teal-500">Blog App</h1>
+      <h1 className=" text-3xl font-semibold text-teal-500">
+        Record : The Blog App
+      </h1>
     </div>
   );
 }
